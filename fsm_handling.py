409cd4b18f6f3e8f582b6b9f90d718f6f1819b78
Strackeror <rremilavergne@gmail.com>
2020-10-18 21:34:34 +0200

    Working Import/Export

--- fsm_handling.py
+++ fsm_handling.py
@@ -1,81 +1,170 @@
 from ast import Str
+from io import BufferedReader, SEEK_SET
 from operator import sub
-import struct
-import json
-import sys
-import base64
-
+from typing import Any
 from construct import *
-from construct.core import Int16ul, Int32ul, Int64ul, evaluate
-from construct.lib.binary import bytes2bits
+from construct.core import Int16ul, Int32sl, Int32ul, Int64ul, evaluate
+
+from dataclasses import dataclass
+import sys
+import json
 
 
-class Pointed(Subconstruct):
-    def __init__(self, subcon, pointed):
-        self.pointed = pointed
+# Queued Pointer Handling
+
+@dataclass
+class PointerQueuedData:
+    pointerOffset: int
+    pointerType: Construct
+    data: Any
+    dataType: Construct
+
+class DataPointer(Subconstruct): 
+    def __init__(self, subcon: Construct, pointed: Construct, tag: str = "ptrData"):
         super().__init__(subcon)
+        self.pointed = pointed
+        self.tag = tag
+    
+    def _parse(self, stream, context, path):
+        ptrVal = super()._parse(stream, context, path)
+        return Pointer(ptrVal, self.pointed)._parse(stream, context, path)
+    
+    def _build(self, obj, stream, context, path):
+        if self.tag not in context._params:
+            context._params[self.tag] =  []
+        context._params[self.tag].append(PointerQueuedData(stream_tell(stream, path), self.subcon, obj, self.pointed))
+        super()._build(0, stream, context, path)
+        return obj
+
+class DataEntries(Construct):
+    def __init__(self, tag: str = "ptrData"):
+        super().__init__()
+        self.tag = tag
+        self.flagbuildnone = True
+
+    def _build(self, obj, stream, context, path):
+        if self.tag not in context._params:
+            return
+
+        for queuedData in context._params[self.tag]:
+            pos = stream_tell(stream, path)
+            stream_seek(stream, queuedData.pointerOffset, SEEK_SET, path)
+            queuedData.pointerType._build(pos, stream, context, path)
+            stream_seek(stream, pos, SEEK_SET, path)
+            queuedData.dataType._build(queuedData.data, stream, context, path)
+        context._params[self.tag].clear()
+        return obj
 
     def _parse(self, stream, context, path):
-        val = super()._parse(stream, context, path)
-        return Pointer(val, self.pointed)._parse(stream, context, path)
-    
-    def _build(self, obj, stream, context, path):
-        currentOffs = context._params.offs
-        ret1 = super()._build(currentOffs, stream, context, path)
-        ptr = Pointer(currentOffs, self.pointed)
-        ptr._build(obj, stream, context, path)
-        currentOffs += len(self.pointed.build(obj))
-        context._params.offs = currentOffs
-        return ret1
-    
-    def _sizeof(self, context, path):
-        if "preOffs" in context._params:
-            return super()._sizeof(context, path)
-        return super()._sizeof(context, path) + self.pointed._sizeof(context, path)
+        pass
 
-class PointerContainer(Subconstruct):
-    def _build(self, obj, stream, context, path):
-        context._params.preOffs = True
-        size = self.subcon._sizeof(context, path)
-        context._params.pop("preOffs")
 
-        context._params.offs = size + stream_tell(stream, path)
-        ret = super()._build(obj, stream, context, path)
-        context._params.pop("offs")
-        return ret
+def PrefixedOffset(sizetype, type, offs = 0):  
+    return FocusedSeq("content",
+        "_data" / Rebuild(Struct(
+            "size" / Rebuild(sizetype, len_(this.data) - offs),
+            "data" / Bytes(this.size + offs)
+        ), lambda obj: {"data": type.build(obj.content, **{**obj._params, **obj})}),
 
+        "content" / RestreamData(this._data.data, type)
+    )
+
+# Class definition handling
 
 ClassMemberDefinition = Struct(
-    "name" / Pointed(Int64ul, CString("utf8")),
+    "name" / DataPointer(Int64ul, CString("utf8"), "names"),
     "type" / Byte,
     "unkn" / Byte,
     "size" / Byte,
-    "unknData" / Byte[69],
+    "_unknData" / Default(Byte[69], [0 for _ in range(69)]),
 )
 
-ClassDefinition = Struct(
-    "hash" / Int64ul,
-    "_memberCount" / Int64ul,
-    "members" / ClassMemberDefinition[this._memberCount]
+ClassDefinition = DataPointer(
+    Int64ul,
+    Struct(
+        "hash" / Int64ul,
+        "members" / PrefixedArray(Int64ul, ClassMemberDefinition)
+    ),
+    "definitionData")
+
+ClassDefinitionList = FocusedSeq(
+    "definitions",
+    "_count" / Rebuild(Int32ul, len_(this.definitions)),
+    "definitions" / Prefixed(
+        Int32ul,
+        Aligned(
+            8,
+            FocusedSeq("definitions",
+                       "definitions" /
+                       ClassDefinition[this._._count],
+                       DataEntries("definitionData"),
+                       DataEntries("names"),
+                       )))
 )
 
+# Hierarchy handling
+varcount = 0
+def varHandling(this):
+    global varcount
+    ret = varcount
+    varcount += 1
+    return ret
 
-ArrayDefinitionList = Struct(
-    count = Int32ul,
-    size = Int32ul,
-    data = Bytes(this.size) 
-)
+def ClassEntry(): 
+    return Struct(
+        "classId" / Int16ul,
+        "_valid" / Computed(lambda this: this.classId // 2 < len(this._root.defs)),
+        "_var" / IfThenElse(
+            this._valid,
+            Rebuild(Int16ul, varHandling),
+            Default(Int16ul, 0)),
+        "content" / If(this._valid,
+            LazyBound(lambda: PrefixedOffset(
+                Int64ul, ClassImplementation(this._._.classId // 2), -8))
+           )
+    )
 
+def ClassImpl(id):
+  return FocusedSeq("classes",
+      "_class" / Computed(lambda this: this._root.defs[evaluate(id, this)]),
+      "classes" / FocusedSeq("entries",
+          "_index" / Index,
+          "_member" / Computed(lambda this: this._._class.members[this._index]),
+          "entries" / Sequence(
+              Computed(this._._member.name),
+              DataEntry(lambda this: this._._._member.type)
+          )
+      )[len_(this._class.members)]
+  )
 
-class ClassDefinitionList(Adapter):
+class ClassImplementation(Adapter):
+    def __init__(self, id):
+        super().__init__(ClassImpl(id))
+
     def _decode(self, obj, context, path):
-        return PointerContainer(Pointed(Int64ul, ClassDefinition)[obj.count]).parse(obj.data)
-
+        return {i[0]:i[1] for i in obj}
+    
     def _encode(self, obj, context, path):
-        data = PointerContainer(Pointed(Int64ul, ClassDefinition)[
-                                len(obj)]).build(obj)
-        return {"count": len(obj), "size": len(data), "data": data}
+        return [[k, v] for (k,v) in obj.items()]
 
+
+def DataEntry(type):
+    return FocusedSeq("values",
+        "_count" / Rebuild(Int32ul, len_(this.values)),
+        "values" / Switch(type, {
+            0: Pass,
+            1: ClassEntry(),
+            2: ClassEntry(),
+            3: Byte,
+            4: Byte,
+            6: Int32ul,
+            10: Int32sl,
+            14: CString("utf8"),
+        }, default=StopFieldError)[this._count],
+    )
+
+
+# Top-level stuff
 Header = Struct(
     "sig" / Byte[4],
     "version" / Int16ul,
@@ -86,25 +175,45 @@ Header = Struct(
 
 topLevel = Struct(
     "header" / Header,
-    "defs" / ClassDefinitionList(ArrayDefinitionList),
+    "defs" / ClassDefinitionList,
+    "root" / ClassEntry()
 )
 
-def encode(path):
-    pass
 
+def filterVariables(node):
+    if isinstance(node, dict):
+        for key in {**node}:
+            if isinstance(key, str) and key.startswith("_"):
+                node.pop(key)
+        for key in node:
+            filterVariables(node[key])
+    if isinstance(node, list):
+        for val in node:
+            filterVariables(val)
+    return
+    
+
+    
 class Encoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, bytes):
             return list(obj)
+        if isinstance(obj, BufferedReader):
+            return []
         return str(obj)
 
 def decode(path):
     with open(path, 'rb') as f:
         main_dict = topLevel.parse_stream(f)
-    with open(path + "redo", 'wb') as f:
-        topLevel.build_stream(main_dict, f)
 
-        #print(json.dumps(main_dict, cls=Encoder, ensure_ascii=False))
+    filterVariables(main_dict)
+    with open(path + "redo", 'wb') as f:
+         topLevel.build_stream(main_dict, f)
+    with open(path + ".json", 'w', encoding="utf-8") as f:
+        json.dump(main_dict, f, cls=Encoder, indent=True, ensure_ascii=False)
+
+def encode(path):
+    pass
 
 target = sys.argv[1]
 if (target.endswith('.json')):
